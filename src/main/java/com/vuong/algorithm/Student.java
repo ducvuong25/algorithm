3ae4f30b1b93d04ea9724a18438e98a2b8787615
ducvuong25 <ducvuong25@gmail.com>
2021-01-06 15:11:40 +0700

    them xin chao

--- src/main/java/com/vuong/algorithm/Student.java
+++ src/main/java/com/vuong/algorithm/Student.java
@@ -4,7 +4,7 @@ public class Student {
     public static void main(String[] args) {
         // viet mot ham in ra tat cac thua so nguyen to cua mot so
 
-
+        System.out.println("xin chao");
     }
 
 
